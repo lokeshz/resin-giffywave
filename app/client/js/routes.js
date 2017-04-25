6bba1c4bc2a3035ca3db03d9b0193100dcbd3a19
Tanmay Dharmaraj <tanmay.dharmaraj@rapidcircle.com>
2017-04-25 23:56:35 +0530

    Added routing. Added typography.

--- app/client/js/routes.js
+++ app/client/js/routes.js
@@ -1,15 +1,19 @@
 angular.module('app.routes', [])
-    .config(['$routeProvider',
-        function($routeProvider) {
-            $routeProvider
-                .when('/', {
-                    templateUrl: "client/tpl/main.html",
-                    controller: "MainController"
-                })
-                .when('/page2', {
-                    templateUrl: "client/tpl/page2.html",
-                    controller: "MainController"
-                })
-                .otherwise('/');
-        }
-    ]);
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {
+                templateUrl: "client/tpl/main.html",
+                controller: "MainController"
+            })
+            .when('/page2', {
+                templateUrl: "client/tpl/page2.html"
+            })
+            .when('/page3', {
+                templateUrl: "client/tpl/page2.html"
+            })
+            .when('/page4', {
+                templateUrl: "client/tpl/page2.html"
+            })
+            .otherwise({
+              template : "<h1>Nothing</h1><p>Nothing has been selected</p>"
+            });
+    }]);
